5c639653a21737715a0cc1bdeb485291245bf1ab
Jacob Okamoto <oko@oko.io>
2019-12-25 18:54:23 -0800

    clean up PR issues

--- plugin/step/sanitycheck/sanity_check.go
+++ plugin/step/sanitycheck/sanity_check.go
@@ -14,7 +14,7 @@ import (
 )
 
 // TypeName is the name of this plugin
-const TypeName = "go2chef.winsanitycheck"
+const TypeName = "go2chef.sanitycheck"
 
 // SanityCheck implements basic sanity checks, and provides an API
 // for additional sanity-checks to be built as plugins.
